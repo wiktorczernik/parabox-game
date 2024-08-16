b7bf70758f1789241f251049edb139f0ddd779d6
Wiktor Czernik <git@czernik.dev>
2024-08-17 01:55:21 +0200

    feat: Added action events and more commenting

--- Assets/Scripts/Local/Components/Switch.cs
+++ Assets/Scripts/Local/Components/Switch.cs
@@ -1,23 +1,38 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Switch : MonoBehaviour, IInteractable
 {
+    #region State
     public bool isActive { get => _isActive; private set => _isActive = value; }
     public bool isBusy { get => _isBusy; private set => _isBusy = value; }
+    #endregion
+    #region Events
+    public event Action onActivate;
+    public event Action onDeactivate;
+    #endregion
 
+    #region Editor State
     [Header("State")]
     [SerializeField] bool _isActive = false;
     [SerializeField] bool _isBusy = false;
+    #endregion
+    #region Unity Events
     [Header("Events")]
     [SerializeField] UnityEvent _onActivateEvent;
     [SerializeField] UnityEvent _onDeactivateEvent;
+    #endregion
+    #region Delays
     [Header("Delays")]
     [SerializeField] float _switchAnimationDelay = 0.4f;
     [SerializeField] float _switchPostDelay = 0.2f;
+    #endregion
+    #region Parameters
     [Header("Parameters")]
     [SerializeField] bool _isActiveDefault = false;
+    #endregion
 
 
     public InteractableHoverResponse GetHoverResponse(IInteractor interactor)
@@ -45,9 +60,15 @@ public class Switch : MonoBehaviour, IInteractable
         isActive = !isActive;
 
         if (isActive)
+        {
             _onActivateEvent.Invoke();
+            onActivate?.Invoke();
+        }
         else
+        {
             _onDeactivateEvent.Invoke();
+            onDeactivate?.Invoke();
+        }
         yield return new WaitForSeconds(_switchPostDelay);
         isBusy = false;
     }
