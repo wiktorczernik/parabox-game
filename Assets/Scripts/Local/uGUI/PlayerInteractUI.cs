43fadf5aa670bdc3f3c7837d431d8ace377693fd
Wiktor Czernik <git@czernik.dev>
2024-08-20 00:05:18 +0200

    improvement: Better interaction UI

--- Assets/Scripts/Local/uGUI/PlayerInteractUI.cs
+++ Assets/Scripts/Local/uGUI/PlayerInteractUI.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 
 public class PlayerInteractUI : MonoBehaviour
 {
-    #region Components
-    [Header("Components")]
-    [SerializeField] GameObject containerInteractUI;
-    [SerializeField] TextMeshProUGUI textInteract;
-    #endregion
+    [SerializeField] GameObject activePoint;
+    [SerializeField] GameObject inactivePoint;
+
+    [SerializeField] InteractableHoverResponse[] responseKeys;
+    [SerializeField] GameObject[] responseValues;
+
 
     private void FixedUpdate()
     {
         PlayerInteractionSeeker seeker = Player.local.GetModule<PlayerInteractionSeeker>();
-        containerInteractUI.SetActive(false);
+
+        activePoint.SetActive(false);
+        inactivePoint.SetActive(false);
+        DisableResponseTextes();
 
         if (seeker.hoveredObject != null)
         {
@@ -22,25 +26,31 @@ public class PlayerInteractUI : MonoBehaviour
 
             if (res != InteractableHoverResponse.None)
             {
-                bool CanInteract = seeker.hoveredObject.CanInteract(Player.local);
+                bool canInteract = seeker.hoveredObject.CanInteract(Player.local);
 
-                if (CanInteract)
+                if (canInteract)
                 {
-                    if (res == InteractableHoverResponse.Enable)
+                    activePoint.SetActive(true);
+                    for (int i = 0; i < responseKeys.Length; i++)
                     {
-                        textInteract.text = "Press (E) to disable";
+                        if (responseKeys[i] == res)
+                        {
+                            responseValues[i].SetActive(true);
+                        }
                     }
-                    else
-                    {
-                        textInteract.text = "Press (E) to enable";
-                    }
-                    containerInteractUI.SetActive(true);
                 }
                 else
                 {
-                    containerInteractUI.SetActive(false);
+                    inactivePoint.SetActive(true);
                 }
             }
+        }
+    }
+    void DisableResponseTextes()
+    {
+        foreach (var resText in responseValues)
+        {
+            resText.SetActive(false);
         }
     }
 }
