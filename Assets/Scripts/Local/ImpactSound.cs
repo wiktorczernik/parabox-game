028366d5cb55d26f05730b9e80c340857448b947
Wiktor Czernik <git@czernik.dev>
2024-08-20 16:17:40 +0200

    fix: Fuck pins

--- Assets/Scripts/Local/ImpactSound.cs
+++ Assets/Scripts/Local/ImpactSound.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ImpactSound : MonoBehaviour
@@ -5,9 +6,16 @@ public class ImpactSound : MonoBehaviour
     public AudioSource audioSource;
     public float minVelocity = 1f;
 
+    bool canPlay = false;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(10f);
+        canPlay = true;
+    }
     void OnCollisionEnter(Collision collision)
     {
-        if (minVelocity > collision.relativeVelocity.magnitude) return;
+        if (minVelocity > collision.relativeVelocity.magnitude || !canPlay) return;
 
         AudioSource.PlayClipAtPoint(audioSource.clip, collision.contacts[0].point, audioSource.volume);
     }
